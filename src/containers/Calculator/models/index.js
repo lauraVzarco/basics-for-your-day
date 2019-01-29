800407fda56fa1ad78711bd54edc03e521a8e40a
lauraVzarco <laurav.zarco@gmail.com>
2019-01-29 12:02:52 +0100

    ready to do immutable

--- src/containers/Calculator/models/index.js
+++ src/containers/Calculator/models/index.js
@@ -1,7 +1,9 @@
-export const calculatorModel = {
+import { Record } from 'immutable';
+
+export const calculatorModel = Record({
   firstOperationNumber: 0,
   secondOperationNumber: 0,
   operator: '',
   result: 0,
   display: 0,
-};
+});
