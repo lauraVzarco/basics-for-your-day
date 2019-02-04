d965cac5fa80a6d90b32f6e05ce4838a337fabe3
lauraVzarco <laurav.zarco@gmail.com>
2019-02-04 16:02:17 +0100

    unify modal

--- src/containers/ToDoList/index.jsx
+++ src/containers/ToDoList/index.jsx
@@ -8,8 +8,7 @@ import InputTodo from './components/InputTodo';
 import TodoListPanel from './components/TodoListPanel';
 import FilterPanel from './components/FilterPanel';
 import ClearButton from './components/ClearButton';
-import ModalClear from './components/ModalClear';
-import ModalExit from './components/ModalExit';
+import Modal from './components/Modal';
 import './style.css';
 import {
   submitTask, pressClear, toggleTask
@@ -29,8 +28,8 @@ class TodoList extends Component {
   state = {
     task: '',
     id: 0,
-    modalClearIsOpen: false,
-    modalExitIsOpen: false
+    modalIsOpen: false,
+    exitIsNeeded: false
   }
 
   handleTask = e => { this.setState({ task: e.target.value }); }
@@ -47,16 +46,20 @@ class TodoList extends Component {
 
   handleClear = () => { this.props.clear(); this.setState({ id: 0 }); }
 
-  handleClearModal = () => { this.setState({ modalClearIsOpen: !this.state.modalClearIsOpen }); }
+  handleModal = () => { this.setState({ modalIsOpen: !this.state.modalIsOpen }); }
 
-  handleExitModal = () => { this.setState({ modalExitIsOpen: !this.state.modalExitIsOpen }); }
+  handleOnClickExit = () => {
+    this.setState({
+      exitIsNeeded: true,
+      modalIsOpen: !this.state.modalIsOpen
+    });
+  };
 
   // eslint-disable-next-line consistent-return
   closeModalWithEsc = (e) => {
     if (e.keyCode === 27) {
       this.setState({
-        modalClearIsOpen: false,
-        modalExitIsOpen: false
+        modalIsOpen: false,
       });
     } else return null;
   }
@@ -78,12 +81,7 @@ class TodoList extends Component {
       <div onKeyDown={ this.closeModalWithEsc }>
         <h1 className="todoTitle">todos</h1>
         <div className="todoClearbutton">
-          <ClearButton handleModal={ this.handleClearModal } />
-          {this.state.modalClearIsOpen === true && this.props.listOfTasks.size >= 1
-            ? <ModalClear handleClear={ this.handleClear }
-              handleClearModal={ this.handleClearModal }
-              closeModalWithEsc={ this.closeModalWithEsc } />
-            : null}
+          <ClearButton handleModal={ this.handleModal } />
         </div>
         <div className="todoContainer">
           <InputTodo
@@ -101,15 +99,20 @@ class TodoList extends Component {
           />
         </div>
         {this.props.listOfTasks.size >= 1
-          ? <button onClick={ this.handleExitModal }> Home </button>
-          : <Link to="/"><button onClick={ this.handleExitModal }>  Home  </button></Link>
+          ? <button onClick={ this.handleOnClickExit }> Home </button>
+          : <Link to="/">
+            <button onClick={ this.handleOnClickExit }>  Home  </button>
+          </Link>
         }
-        {this.state.modalExitIsOpen === true && this.props.listOfTasks.size >= 1
-          ? <ModalExit
-            handleExitModal={ this.handleExitModal }
-            closeModalWithEsc={ this.closeModalWithEsc } />
-          : null
-        }
+        <div>
+          {this.state.modalIsOpen === true && this.props.listOfTasks.size >= 1
+            ? <Modal
+              handleClear={ this.handleClear }
+              handleModal={ this.handleModal }
+              handleExit={ this.state.exitIsNeeded } />
+            : null}
+        </div>
+
       </div >
     );
   }
