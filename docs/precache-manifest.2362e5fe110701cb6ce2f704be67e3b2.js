64f1c5bfa88525ee4fe5b97d3409562cdc4d7685
lauraVzarco <laurav.zarco@gmail.com>
2019-02-06 16:39:42 +0100

    final commit

---
+++ docs/precache-manifest.2362e5fe110701cb6ce2f704be67e3b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bf2021dda6a1a8a1c68",
+    "url": "/static/css/main.5e0f3045.chunk.css"
+  },
+  {
+    "revision": "1bf2021dda6a1a8a1c68",
+    "url": "/static/js/main.1bf2021d.chunk.js"
+  },
+  {
+    "revision": "6dc37c682ed1b9722b27",
+    "url": "/static/js/1.6dc37c68.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9aecd722536af2e90ed8102b881347ff",
+    "url": "/static/media/modalbackground.9aecd722.png"
+  },
+  {
+    "revision": "a8f9a2fb5cae26330c82f19ed81a37f1",
+    "url": "/index.html"
+  }
+];
